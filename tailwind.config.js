7279caedee148bd5680738aa10c61b238edcaf7d
“sherlock6147” <tooshort9541@gmail.com>
2023-07-21 19:46:09 +0530

    v1 ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./manzer_alam_main_website/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  theme: {extend: {
+    fontFamily: {
+      'ubuntu-mono': ["'Ubuntu Mono'",'mono','sans-serif'],
+      'gugi': ["'Gugi'",'mono','sans-serif'],
+      'raleway': ["'Raleway'",'sans-serif'],
+    }
+  },
   },
   plugins: [],
 }
